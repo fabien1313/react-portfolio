55a74f266df57f839a423800c7ed090a5b9b958a
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-04 14:03:12 -0500

    adjustments to header/about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import image from '../images/profile.png'
 
 const About = () => {
   return (
     <div id="about-container">
       <h2 id="about-title">About Me</h2>
+      <div id="about-text-pic-container">
       <p id="about-text">
-      As a passionate software engineering enthusiast, I am thrilled to offer my expertise and innovative problem-solving skills. With a strong foundation in computer science and programming principles, I am confident that I can create cutting-edge solutions that make a difference in people's lives. I have experience in relevant skills and technologies, and I am a quick learner who thrives in fast-paced, dynamic environments. In addition to my technical skills, I value collaboration and communication in the workplace, which I believe are crucial to delivering successful projects. I am committed to working hard and contributing my skills to help the team achieve its goals. I am excited to embark on this journey and make a meaningful impact in the software engineering world.
+      Hi! My name is <span id="name">Fabien</span>. I'm a highly motivated individual who has continued to pursue the software engineering path. I truly enjoy discovering new methods and technologies to create digital products. Through my college experience at Texas State University and coding bootcamp at The University of Texas, Austin, I have been exposed to the power and innovation coding possesses. I have learned to harness the power of coding through consistent execution and trial and error. I am eager to work with other like minded individuals to challenge, share and colaborate on unique ideas. 
+      <br/> 
+      <br/>
+      I am currently looking for a software engineering position. Also, I am open to freelance work and would love to network and build connections. Enjoy my portfolio and feel free to contact me below! 
+      
       </p>
+      <img id= "profile-pic" src={ image } alt="" />
+      </div>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,20 @@ import gIcon from '../images/github.png';
 const Header = () => {
   return (
     <header id="header">
-      <h1 id="name-plate">Fabien Moreno</h1>
-      <h2 id="role-title">Full-Stack Web Developer</h2>
     <img id="avatar" src= { image } alt="profile picture" />
+    <h1 id="name-plate">Fabien Moreno</h1>
+    <h2 id="role-title">Full-Stack Web Developer</h2>
       <nav id="navbar">
           <li className="nav-item"><a className="anchor-item" href="/about">About Me</a></li>
           <li className="nav-item"><a className="anchor-item" href="/portfolio">Portfolio</a></li>
           <li className="nav-item"><a className="anchor-item" href="/resume">Resume</a></li>
           <li className="nav-item"><a className="anchor-item" href="/contact">Contact</a></li>
       </nav>
+      <div id="icon-container">
     <a href="https://twitter.com/CodeFabien" target="_blank"><img id="tIcon" src= { tIcon } alt="twitter icon" /></a>
     <a href="https://www.linkedin.com/in/fabien-moreno/" target="_blank"><img id="lIcon" src= { lIcon } alt="linkedin icon" /></a>
     <a href="https://github.com/fabien1313" target="_blank"><img id="gIcon" src= { gIcon } alt="linkedin icon" /></a>
+      </div>
 
     </header>
     
