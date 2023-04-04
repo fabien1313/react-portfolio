c84308936f6e2ed183d26da4bd0a9659bd7bcbb3
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-04 02:03:18 -0500

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import image from '../images/reactatar.png';
+import background from '../images/background1.jpg';
+
+const style = {
+  backgroundImage: `url(${background})`,
+  backgroundSize: 'cover',
+}
 
 const Header = () => {
   return (
-    <header>Header
-      <nav>
-        <ul>
-          <li><a href="/about">About Me</a></li>
-          <li><a href="/portfolio">Portfolio</a></li>
-          <li><a href="/resume">Resume</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
+    <header id="header">
+      <h1 id="name-plate">Fabien Moreno</h1>
+      <h2 id="role-title">Full-Stack Web Developer</h2>
+    <img id="avatar" src= { image } alt="profile picture" />
+      <nav id="navbar">
+          <li className="nav-item"><a className="anchor-item" href="/about">About Me</a></li>
+          <li className="nav-item"><a className="anchor-item" href="/portfolio">Portfolio</a></li>
+          <li className="nav-item"><a className="anchor-item" href="/resume">Resume</a></li>
+          <li className="nav-item"><a className="anchor-item" href="/contact">Contact</a></li>
       </nav>
     </header>
     
