b44405bf3e360fd1d4a1ca7f312273aee0fe6e30
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-05 22:05:17 -0500

    contact form working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-const Contact = () => {
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7ojuod9', 'template_phfyjtk', form.current, '6Aalk2cnYct2LfguC')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
-    <div></div>
-  )
-}
+    <form id="contact-form-container" ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
 
 export default Contact
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import credentials from '../images/resume.pdf'
 
 const Resume = () => {
   return (
-    <div></div>
+    <section id="resume-container">
+      <h2 id="resume-title">Resume</h2>
+
+      <div id="cv-btn-container">
+      <a href={credentials} download><button id="cv-btn">Download CV</button></a>
+      </div>
+       
+
+    </section>
   )
 }
 
