5f0406553738250588ca892fae66cdd5f35f1a0c
fabien moreno <fabienmoreno1331@yahoo.com>
2023-04-06 21:12:24 -0500

    captions on the projects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import { useState , useEffect} from 'react'
 import image from '../images/reactatar.png';
 import tIcon from '../images/twitter.png';
 import lIcon from '../images/linkedin.png';
@@ -8,28 +8,83 @@ import gIcon from '../images/git.png';
 const Header = () => {
 
   const [nav, setNav] = useState("about-container");
-  const handleClick = (link) => {setNav(link)};
-  
+
+  useEffect(() => {
+    setNav("about-container");
+  }, []);
+
+  const handleClick = (link) => {
+    setNav(link);
+  };
+
   return (
     <header id="header">
-    <img id="avatar" src= { image } alt="profile picture" />
-    <h1 id="name-plate">Fabien Moreno</h1>
-    <h2 id="role-title">Full-Stack Web Developer</h2>
+      <img id="avatar" src={image} alt="profile picture" />
+      <h1 id="name-plate">Fabien Moreno</h1>
+      <h2 id="role-title">Full-Stack Web Developer</h2>
       <nav id="navbar">
-          <li className="nav-item"><a className={`anchor-item ${nav === "about-container" ? "current-page" : ""}`} title="About" onClick={() => handleClick("about-container")} href="#about-container">About Me</a></li>
-          <li className="nav-item"><a className={`anchor-item ${nav === "portfolio-container" ? "current-page" : ""}`} title="Portfolio" onClick={() => handleClick("portfolio-container")} href="#portfolio-container">Portfolio</a></li>
-          <li className="nav-item"><a className={`anchor-item ${nav === "resume-container" ? "current-page" : ""}`} title="Resume" onClick={() => handleClick("resume-container")} href="#resume-container">Resume</a></li>
-          <li className="nav-item"><a className={`anchor-item ${nav ==="contact-w-photo-div" ? "current-page" : ""}`} title="Contact" onClick={() => handleClick("contact-w-photo-div")} href="#contact-w-photo-div">Contact</a></li>
+        <li className="nav-item">
+          <a
+            className={`anchor-item ${
+              nav === "about-container" ? "current-page" : ""
+            }`}
+            title="About"
+            onClick={() => handleClick("about-container")}
+            href="#about-container"
+          >
+            About Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={`anchor-item ${
+              nav === "portfolio-container" ? "current-page" : ""
+            }`}
+            title="Portfolio"
+            onClick={() => handleClick("portfolio-container")}
+            href="#portfolio-container"
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={`anchor-item ${
+              nav === "resume-container" ? "current-page" : ""
+            }`}
+            title="Resume"
+            onClick={() => handleClick("resume-container")}
+            href="#resume-container"
+          >
+            Resume
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={`anchor-item ${
+              nav === "contact-w-photo-div" ? "current-page" : ""
+            }`}
+            title="Contact"
+            onClick={() => handleClick("contact-w-photo-div")}
+            href="#contact-w-photo-div"
+          >
+            Contact
+          </a>
+        </li>
       </nav>
       <div id="icon-container">
-    <a href="https://twitter.com/CodeFabien" target="_blank"><img id="tIcon" src= { tIcon } alt="twitter icon" /></a>
-    <a href="https://github.com/fabien1313" target="_blank"><img id="gIcon" src= { gIcon } alt="linkedin icon" /></a>
-    <a href="https://www.linkedin.com/in/fabien-moreno/" target="_blank"><img id="lIcon" src= { lIcon } alt="linkedin icon" /></a>
+        <a href="https://twitter.com/CodeFabien" target="_blank">
+          <img id="tIcon" src={tIcon} alt="twitter icon" />
+        </a>
+        <a href="https://github.com/fabien1313" target="_blank">
+          <img id="gIcon" src={gIcon} alt="linkedin icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/fabien-moreno/" target="_blank">
+          <img id="lIcon" src={lIcon} alt="linkedin icon" />
+        </a>
       </div>
-
     </header>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import thedailydose from '../images/thedailydose.png'
-import socialDose from '../images/socialdose.png'
-import workDay from '../images/workday.png'
+import thedailydose from '../images/thedailydose.png';
+import socialDose from '../images/socialdose.png';
+import workDay from '../images/workday.png';
+import weatherDash from '../images/weatherdash.png';
+import noteTaker from '../images/notetaker.png';
+import passGen from '../images/passgen.png';
 
 const Portfolio = () => {
   return (
     <section id="portfolio-container">
       <h2 id="portfolio-title">Portfolio</h2>
-
+  
       <div className="project-container">
         <p className="project-text">As a developer, I understand the importance of staying up-to-date with technical concepts, advancements, and new technologies. That's why I decided to build a CMS-style blog site that serves as a platform for developers to share their ideas and insights with one another.
 
@@ -17,7 +20,10 @@ const Portfolio = () => {
 
         Overall, my CMS-style blog site is a valuable resource for developers looking to share their knowledge and connect with others in the field.
         </p>
-        <img className="project-photo"  src={ thedailydose } alt="Tech Blog Project" />
+        <figure>
+        <figcaption>The Daily Dose</figcaption>
+        <img className="project-photo" src={ thedailydose } alt="Tech Blog Project"/>
+        </figure>
       </div>
 
       <div className="btn-div">
@@ -34,7 +40,10 @@ const Portfolio = () => {
 
         Overall, the API I built for this social network web application provides a foundation for a full-stack application that can be used to connect users and facilitate sharing of thoughts and ideas. It showcases my proficiency in using technologies commonly used in social networking platforms, such as Express.js and MongoDB.
         </p>
+        <figure>
+          <figcaption>Social Dose</figcaption>
         <img className="project-photo" src={ socialDose } alt="Social Network API" />
+        </figure>
       </div>
 
       <div className="btn-div">
@@ -51,12 +60,56 @@ const Portfolio = () => {
 
         Overall, the calendar application I built showcases my skills in front-end development and provides a valuable tool for managing daily tasks and events.
         </p>
-
+        <figure>
+          <figcaption>Workday Schedular</figcaption>
         <img className="project-photo" src={ workDay} alt="Work Day Schedular" />
+        </figure>
       </div>
 
       <div className="btn-div">
       <a href="https://fabien1313.github.io/dayscheduler/" target="_blank"><button id="demo-btn">Live Demo</button></a> <a href="https://github.com/fabien1313/dayscheduler" target="_blank"><button id="repo-btn">Repository</button></a>
+      </div>
+
+      <div className="project-container">
+        <p className="project-text">
+        This project involves building a weather dashboard that retrieves weather data from a third-party API and displays it in a user-friendly format. The dashboard runs in a web browser and features dynamically updated HTML and CSS, providing users with up-to-date weather information for various cities. The importance of this project lies in its ability to help users plan their activities and stay informed about weather conditions in different locations. By leveraging the data provided by the 5 Day Weather Forecast API, developers can create a dashboard that is both reliable and accurate. The dashboard can display key weather metrics such as temperature, humidity, and wind speed, as well as provide visual representations such as charts and graphs to help users easily interpret the data. This project is an excellent demonstration of a developer's ability to work with third-party APIs and create a useful and visually appealing application for end-users.
+        </p>
+        <figure>
+          <figcaption>Weather Dashboard</figcaption>
+        <img className="project-photo" src={ weatherDash} alt="Weather Dashboard Application" />
+        </figure>
+      </div>
+
+      <div className="btn-div">
+      <a href="https://fabien1313.github.io/weather-application/" target="_blank"><button id="demo-btn">Live Demo</button></a> <a href="https://github.com/fabien1313/weather-application" target="_blank"><button id="repo-btn">Repository</button></a>
+      </div>
+
+      <div className="project-container">
+        <p className="project-text">
+        This project involves building a note-taking application that uses an Express.js back end to save and retrieve note data from a JSON file. The application allows users to write and save notes, providing a convenient way to organize and track their ideas, thoughts, and reminders. The importance of this project lies in its ability to help users manage their workflow and increase productivity. With the ability to easily create and store notes, users can quickly capture ideas, prioritize tasks, and keep track of important information. The Express.js back end provides a fast and reliable way to store and retrieve note data, ensuring that users can access their notes whenever and wherever they need them. This project is an excellent example of a developer's ability to create a robust and functional application that meets the needs of end-users, leveraging the power of Express.js to provide a seamless and efficient note-taking experience.
+        </p>
+      <figure>
+        <figcaption>Note Taker</figcaption>
+        <img className="project-photo" src={ noteTaker} alt="Note Taker Application" />
+      </figure>
+      </div>
+
+      <div className="btn-div">
+      <a href="https://fabien1313-noteapp.herokuapp.com/" target="_blank"><button id="demo-btn">Live Demo</button></a> <a href="https://github.com/fabien1313/Note_taking_app" target="_blank"><button id="repo-btn">Repository</button></a>
+      </div>
+
+      <div className="project-container">
+        <p className="project-text">
+        This project involves building a password generator application that runs in a web browser and generates a random password based on criteria selected by the user. The application features a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes. The importance of this project lies in its ability to provide users with a secure and reliable way to generate strong passwords that meet their specific requirements. With the increasing importance of online security, it is crucial for individuals and organizations to use strong and unique passwords for their accounts. This password generator application leverages the power of JavaScript to generate random passwords that can include a mix of uppercase and lowercase letters, numbers, and special characters. The user can select the length of the password and which character types to include, providing a flexible and customizable solution to password generation. This project is an excellent example of a developer's ability to create a functional and user-friendly application that meets the needs of end-users, contributing to the overall goal of improving online security.
+        </p>
+      <figure>
+        <figcaption>Password Generator</figcaption>
+        <img className="project-photo" src={ passGen } alt="Work Day Schedular" />
+      </figure>
+      </div>
+
+      <div className="btn-div">
+      <a href="https://fabien1313.github.io/PassGenJS/" target="_blank"><button id="demo-btn">Live Demo</button></a> <a href="https://github.com/fabien1313/PassGenJS" target="_blank"><button id="repo-btn">Repository</button></a>
       </div>
 
     
